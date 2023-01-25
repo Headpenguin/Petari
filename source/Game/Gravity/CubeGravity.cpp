0f3b23a0df2cd5fbab7dc01edd51f73b7f3c1b9e
Headpenguin <headpenguin.penguinarts@gmail.com>
2023-01-24 20:29:49 -0500

    Halfway through decompiling a calcgravityarea

--- source/Game/Gravity/CubeGravity.cpp
+++ source/Game/Gravity/CubeGravity.cpp
@@ -32,3 +32,71 @@ void CubeGravity::setCube(const TPos3f & cube) {
 	
 	updateIdentityMtx();
 }
+
+void CubeGravity::updateMtx(const TPos3f &rMtx) {
+	_58.concat(rMtx, _28);
+	TVec3f dir;
+	_58.getXDir(dir);
+	_88 = VECMag(dir.toCVec());
+	_58.getYDir(dir);
+	_8C = VECMag(dir.toCVec());
+	_58.getZDir(dir);
+	_90 = VECMag(dir.toCVec());
+}
+
+bool CubeGravity::calcOwnGravityVector(TVec3f *pDest, f32 *pScalar, const TVec3f &rPosition) const {
+	//Stack size: 0x20 (8 for lr + sp => 0x1c for data)
+	calcGravityArea(rPosition);
+	
+}
+
+void CubeGravity::calcGravityArea(const TVec3f &rPosition) const {
+	//Stack size: 0x70
+	const TVec3f dirX, dirY, dirZ, trans, _8;
+	_58.getXDir(dirX);
+	_58.getYDir(dirY);
+	_58.getZDir(dirZ);
+	_58.getTrans(trans);
+	_8 = dirZ - rPosition;
+	u8 sum = 0;
+	float xDot = _8.dot(dirX) / _88, yDot = _8.dot(dirY) / _8C, zDot = _8.dot(dirZ);
+	if(xDot >= -_88) { // bc succeeds
+		if(xDot <= _88) { // bc fails
+			sum += 1;
+			//goto ydot comp
+		}
+		else if(mActiveFaces & 1 == 1) {
+			sum += 2;
+			//goto ydot comp
+		}
+		else return -1;
+	}
+	else if(mActiveFaces & 2 == 2) { // bc succeeds
+		sum += 0;
+		// goto ydot comp
+	}
+	else return -1;
+	
+		//r3 <- mActiveFaces & 1
+	if(yDot >= -_8C) { // bc succeeds
+
+	}
+	else if(mActiveFaces & 8 == 8) { // bc succeeds
+		if(zDot >= -_90) { // bc succeeds
+			if(zDot > _90) { // bc succeeds
+				if(mActiveFaces & 16) { // bc succeeds
+					sum += 12;
+					
+				}
+			}
+			else {
+
+			}
+		}
+		else 
+	}
+	else {
+		
+	}
+	return sum;
+}
